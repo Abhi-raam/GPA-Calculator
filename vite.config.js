cb4027fd7b56b2aa431a345d46472c17b09de579
Abhiram Suresh <abhiramunni246@gmail.com>
2023-11-19 02:12:45 +0530

    pwa correction

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // VitePWA({
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "logo512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //   },
+    // }),
+  ],
   server: {
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
-})
+});
