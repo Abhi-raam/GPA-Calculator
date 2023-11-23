fc12b6db5635c1f5fc93de0da0a2433a5095f8c5
Abhiram Suresh <abhiramunni246@gmail.com>
2023-11-24 03:40:44 +0530

    maskable image deleted

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA  } from "vite-plugin-pwa";
 
 const manifestForPlugin = {
 	registerType: "prompt",
-	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked.png"],
+	includeAssets: ["favicon.ico", "apple-touch-icon.png"],
 	manifest: {
     "short_name": "ScoreMate",
     "name": "ScoreMate",
